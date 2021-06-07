c80ed0448729dddd2f953cb879b24afc39ea6992
Rob Zofchak <r.zofchak8@gmail.com>
2021-06-07 11:23:17 -0400

    Updated script

--- bin/convert.sh
+++ bin/convert.sh
@@ -5,14 +5,20 @@ echo "Running Spotify to Discogs script..."
 
 # Log runs to make sure that it does on startups
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
-echo "$TIMESTAMP - program was run " >> /{PATH}/{TO}/{FOLDER}/log/script_log.txt
+echo "$TIMESTAMP - program was run" >> /{PATH}/{TO}/{FOLDER}/logs/script.log
 
 # Move into program directory
 cd /{PATH}/{TO}/{FOLDER}
 
 # Activate environment and run
 source /{PATH}/{TO}/{FOLDER}/env/bin/activate
-/{PATH}/{TO}/{FOLDER}/env/bin/python3 main.py
+if /{PATH}/{TO}/{FOLDER}/env/bin/python3 main.py ; then
+    TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+    echo "$TIMESTAMP - program succeded" >> /{PATH}/{TO}/{FOLDER}/logs/script.log
+else
+    TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+    echo "$TIMESTAMP - program failed" >> /{PATH}/{TO}/{FOLDER}/logs/script.log
+fi
 
 # Deactivate environment and keep terminal open
 deactivate
